ae1b576fee0605c739106ed010036ba4014a8ec6
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-04-19 15:33:11 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+// Init Middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
+app.get('/', (req, res) => res.send('API is running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
