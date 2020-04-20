43687bbf73cd3b54c69de332316ba3a7e5eea919
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-04-20 14:43:03 -0400

    Init React App. Added npm scripts

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import 'bootstrap/scss/bootstrap.scss';
+import './App.scss';
+
+const App = () => (
+  <Fragment>
+    <div className="container">
+    container
+    </div>
+    <div className="container-fluid">
+    container-fluid
+    </div>
+    <h1>App</h1>
+  </Fragment>
+);
+
+export default App;
