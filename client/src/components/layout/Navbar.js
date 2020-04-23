15d2e49f5744e64080c05281a18f45a4ecd07f48
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-04-23 11:07:44 -0400

    Added nav items

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,7 +10,10 @@ export default class Navbar extends React.Component {
             <Link className="nav-link active" to="/search">Job Search</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/">Link</Link>
+            <Link className="nav-link" to="/">About us</Link>
+          </li>
+          <li className="nav-item">
+          <Link className="btn btn-outline-primary rounded-pill" to="/register" role="button">Support Us</Link>
           </li>
         </ul>
       </nav>
